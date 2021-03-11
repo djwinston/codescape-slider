396e5f214a3fc1bcc6e8299daf57969e626b5677
Andrew Schastlivcev <andrew.schastlivcev@sparkybit.com>
2021-03-11 15:30:23 +0200

    Update widget

--- src/direflow-components/codescape-slider/App.js
+++ src/direflow-components/codescape-slider/App.js
@@ -1,69 +1,102 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
-import { /* EventContext, */ Styled, useExternalSource } from 'direflow-component'
+import {
+  /* EventContext, */ Styled /* , useExternalSource */,
+} from 'direflow-component'
 import theme from '../../style/index.scss'
-import bulmaCarousel from 'bulma-carousel/dist/js/bulma-carousel.min.js'
+import Carousel from 'react-simply-carousel'
+// import { Get } from 'react-axios';
 
 const App = (props) => {
+  const [activeSlideIndex, setActiveSlideIndex] = useState(0)
 
-  const hasLoaded = useExternalSource('https://code.jquery.com/jquery-3.3.1.slim.min.js');
-  if (!hasLoaded) {
-    return null;
-  } else {console.log(hasLoaded);
-  }
-  // const dispatch = useContext(EventContext);
-
-  // const handleClick = () => {
-  //   const event = new Event('my-event');
-  //   dispatch(event);
-  // };
-
-  // const renderSampleList = props.sampleList.map((sample) => (
-  //   <div key={sample} className='sample-text'>
-  //     → {sample}
-  //   </div>
-  // ));
-  // Initialize all elements with carousel class.
-
-  // Initialize all div with carousel class
-  const options = {
-    slidesToScroll: 1,
-    slidesToShow: 4,
-  }
-  const doc = document.querySelector('.carousel')
-  var carousels = bulmaCarousel.attach('.carousel', options)
-  console.log(carousels, doc)
-
-  // Loop on each carousel initialized
-  for (var i = 0; i < carousels.length; i++) {
-    // Add listener to  event
-    carousels[i].on('before:show', (state) => {
-      console.log(state)
-    })
+  const setActiveSlideIndexAction = (newActiveSlideIndex) => {
+    setActiveSlideIndex(newActiveSlideIndex)
   }
 
-  // Access to bulmaCarousel instance of an element
-  var element = document.querySelector('.carousel')
-  if (element && element.bulmaCarousel) {
-    // bulmaCarousel instance is available as element.bulmaCarousel
-    element.bulmaCarousel.on('before-show', function (state) {
-      console.log(state)
-    })
+  const Slide = (src) => {
+    return (
+      <div style={{ width: 640, height: 480 }}>
+        <img src="https://picsum.photos/640/480?random=3" alt="" />
+      </div>
+    )
   }
+
+  const Comp = () => {
+    return <div>React Developer</div>
+  }
+
   return (
     <Styled styles={theme}>
       <div className="app">
         <div>{props.componentTitle}</div>
         <button className="button is-primary">Button</button>
-        <section className="section slider">
-          <div className="container">
-            <div id="carousel-demo" className="carousel">
-              <div className="item-1">xxx</div>
-              <div className="item-2">xxx</div>
-              <div className="item-3">xxx</div>
-            </div>
+        <Carousel
+          activeSlideIndex={activeSlideIndex}
+          onRequestChange={setActiveSlideIndexAction}
+          itemsToShow={1}
+          itemsToScroll={1}
+          speed={400}
+          easing="ease-in-out"
+          updateOnItemClick
+          containerProps={{
+            style: {
+              width: '100%',
+              justifyContent: 'space-between',
+            },
+          }}
+          forwardBtnProps={{
+            children: '>',
+            style: {
+              width: 60,
+              height: 60,
+              minWidth: 60,
+              alignSelf: 'center'              
+            },
+            className: 'button is-info'
+          }}
+          backwardBtnProps={{
+            children: '<',
+            style: {
+              width: 60,
+              height: 60,
+              minWidth: 60,
+              alignSelf: 'center',
+            },
+            className: 'button is-info'
+          }}
+        >
+          <div style={{ width: 640, height: 480 }}>
+            <Comp />
           </div>
-        </section>
+          <div style={{ width: 640, height: 480 }}>
+            <img src="https://picsum.photos/640/480?random=2" alt="" />
+          </div>
+          <div style={{ width: 640, height: 480 }}>
+            <img src="https://picsum.photos/640/480?random=3" alt="" />
+          </div>
+          <div style={{ width: 640, height: 480 }}>
+            <img src="https://picsum.photos/640/480?random=4" alt="" />
+          </div>
+          <div style={{ width: 640, height: 480 }}>
+            <img src="https://picsum.photos/640/480?random=5" alt="" />
+          </div>
+          <div style={{ width: 640, height: 480 }}>
+            <img src="https://picsum.photos/640/480?random=6" alt="" />
+          </div>
+          <div style={{ width: 640, height: 480 }}>
+            <img src="https://picsum.photos/640/480?random=7" alt="" />
+          </div>
+          <div style={{ width: 640, height: 480 }}>
+            <img src="https://picsum.photos/640/480?random=8" alt="" />
+          </div>
+          <div style={{ width: 640, height: 480 }}>
+            <img src="https://picsum.photos/640/480?random=9" alt="" />
+          </div>
+          <div style={{ width: 640, height: 480 }}>
+            <img src="https://picsum.photos/640/480?random=10" alt="" />
+          </div>
+        </Carousel>
       </div>
     </Styled>
   )
